bb367af8f62a49076e60a512309c1804ad24704e
shayanel <shayanelhoque@gmail.com>
2026-09-20 18:50:00 -0500

    security: add bounds-safe bigint-buffer compatibility package

---
+++ vendor/bigint-buffer/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+
+export declare function toBigIntLE(buf: Buffer | Uint8Array): bigint;
+export declare function toBigIntBE(buf: Buffer | Uint8Array): bigint;
+export declare function toBufferLE(num: bigint, width: number): Buffer;
+export declare function toBufferBE(num: bigint, width: number): Buffer;
